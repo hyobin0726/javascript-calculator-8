c60391cf75976bddf00af85eac7b39303fedcf52
hyobin0726 <hyobin0726@naver.com>
2025-11-16 22:03:20 +0900

    feat: 문자열 구분자 처리, 합계 구하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    // 문자열 입력
+    const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요. \n");
+
+    // 빈 문자열 처리
+    if (input.trim() === "") {
+      Console.print("결과 : 0");
+      return;
+    }
+
+    // , 또는 : 구분자 처리
+    const numbers = input.split(/,|:/).map(Number);
+    
+    let sum = 0;
+    for (const number of numbers) { //todo: reduce와 비교
+      sum += number;
+    }
+    
+    Console.print(sum)
+
+
+    // 커스텀 구분자 처리
+    
+    // 문자열 계산
+
+    // 결과 출력
+  }
 }
 
 export default App;
